1859cf658b017fc8b6166cba6d4a3523ceae4c2d
hmh86116 <hmh86116@gmail.com>
2022-01-22 04:19:00 +0900

    20220122 - vue Sample 첫 생성 ( vue 첫 사용)

---
+++ 2 attribute Binding/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        image: './../asserts/vmSocks-green.jpg',
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'    }
+})
